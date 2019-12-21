4c68a88e58eadc2b59362b27ce74f3f661a07a5b
zhouxin <cgcg96@sjtu.edu.cn>
2019-12-21 14:12:09 +0800

    feat: update user info & password

--- uranus_middleware/resources/user.py
+++ uranus_middleware/resources/user.py
@@ -1,10 +1,13 @@
+from http import HTTPStatus
+
 from flask import Blueprint
 
-from flask_jwt_simple import create_jwt
+from flask_jwt_simple import jwt_required
 
 from flask_restful import Api, Resource, reqparse
 
-from uranus_middleware.auth_utils import admin_required
+from uranus_middleware.auth_utils import admin_required, get_user_id
+from uranus_middleware.error import error
 from uranus_middleware.models.user import User as UserModel
 
 user_blueprint = Blueprint('user', __name__)
@@ -18,25 +21,29 @@ class User(Resource):
             'value': UserModel.find()
         }
 
-    def post(self):
+    @jwt_required
+    def put(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('role', choices=('administrator', 'staff', 'passenger'), required=True)
-        parser.add_argument('name', required=True)
-        parser.add_argument('password', required=True)
-        parser.add_argument('email', required=True)
-        parser.add_argument('id_number', required=True)
+        parser.add_argument('name', required=False)
+        parser.add_argument('first_name', required=False)
+        parser.add_argument('last_name', required=False)
+        parser.add_argument('email', required=False)
+        parser.add_argument('id_number', required=False)
         data = parser.parse_args()
-        # check if email duplicated
-        users = UserModel.find({'User.email': data['email']})
-        if len(users) > 0:
-            return {
-                'message': 'email duplicated'
-            }, 400
-        user = UserModel(data['name'], data['password'], data['id_number'], data['email'], data['role'])
-        identity = user.save()
-        return {
-            'token': create_jwt(identity)
-        }, 201
+        user_id = get_user_id()
+        return UserModel.update(user_id, data)
 
 
-user_api.add_resource(User, '/user')
+class UserPassword(Resource):
+    @jwt_required
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('password', required=True)
+        data = parser.parse_args()
+        data['password'] = UserModel.generate_digest(data['password'])
+        user_id = get_user_id()
+        return UserModel.update(user_id, data) or error(HTTPStatus.NOT_FOUND)
+
+
+user_api.add_resource(User, '/users')
+user_api.add_resource(UserPassword, '/users/password')
