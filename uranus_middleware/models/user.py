f64e0b916855319093131f9d37b6ffc6694244b8
zhouxin <cgcg96@sjtu.edu.cn>
2019-12-21 14:10:48 +0800

    fix: set 'id_number' as primary key & add fields

--- uranus_middleware/models/user.py
+++ uranus_middleware/models/user.py
@@ -1,18 +1,28 @@
 from dataclasses import dataclass
+from enum import Enum
 from hashlib import md5
 
 from uranus_middleware.models.model import Model
 
 
+class Role(Enum):
+    ADMINISTRATOR = 'administrator'
+    STAFF = 'staff'
+    SECURITY = 'security'
+    PASSENGER = 'passenger'
+
+
 @dataclass
 class User(Model):
     name: str
+    first_name: str
+    last_name: str
     password: str
     id_number: str
     email: str
     role: str
 
-    __slots__ = ('name', 'password', 'id_number', 'email', 'role')
+    __slots__ = ('name', 'first_name', 'last_name', 'password', 'id_number', 'email', 'role')
 
     def __post_init__(self):
         self.password = md5(self.password.encode('utf-8')).hexdigest()
@@ -20,3 +30,7 @@ class User(Model):
     @staticmethod
     def verify_digest(password: str, digest: str) -> bool:
         return md5(password.encode('utf-8')).hexdigest() == digest
+
+    @staticmethod
+    def generate_digest(password: str) -> str:
+        return md5(password.encode('utf-8')).hexdigest()
