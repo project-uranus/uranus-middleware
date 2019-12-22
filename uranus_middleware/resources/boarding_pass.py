e9829ebb3a54117ad18f2073538497e4b69f8323
zhouxin <cgcg96@sjtu.edu.cn>
2019-12-23 01:54:25 +0800

    fix: use flight id for boarding pass get & put

--- uranus_middleware/resources/boarding_pass.py
+++ uranus_middleware/resources/boarding_pass.py
@@ -16,6 +16,11 @@ class BoardingPass(Resource):
 
     @passenger_required
     def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('flight_id', required=True)
+        data = parser.parse_args()
+        boarding_pass = BoardingPassModel.find({'Pass.passenger.flight.id': data['flight_id']})[0]
+        # encode boarding pass
         return {
             'token': 'M1EWING/SHAUN MR       1A11A1 BNESYDQF 551  107Y26J 37    00'
         }
@@ -24,9 +29,9 @@ class BoardingPass(Resource):
     def put(self):
         parser = reqparse.RequestParser()
         parser.add_argument('passenger_status', required=True)
-        parser.add_argument('passenger_id', required=True)
+        parser.add_argument('flight_id', required=True)
         data = parser.parse_args()
-        boarding_pass = BoardingPassModel.find({'Pass.passenger.id': data['passenger_id']})[0]
+        boarding_pass = BoardingPassModel.find({'Pass.passenger.flight.id': data['flight_id']})[0]
         BoardingPassModel.update(boarding_pass.get('id'), {'passenger_status': int(data['passenger_status'])})
         if data['passenger_status'] == PassengerStatus.BOARDING_DATA_REVALIDATION_DONE.value:
             notification.notify(int(data['passenger_id']), 'Boarding data revalidation done, wish you a good trip :)')
