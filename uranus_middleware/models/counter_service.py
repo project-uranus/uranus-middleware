14830ecef6a17b3e971cec01ffdfd2cc5d114a24
zhouxin <cgcg96@sjtu.edu.cn>
2019-12-22 23:14:33 +0800

    feat: counter service

---
+++ uranus_middleware/models/counter_service.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from random import choice
+
+
+@dataclass
+class Counter(object):
+    counter_id: int
+    staff_id: str
+    is_available: bool
+    length: int
+
+    __slots__ = ('counter_id', 'staff_id', 'is_available', 'length')
+
+
+class CounterService(object):
+    def __init__(self):
+        self.__counters = {}  # key: counter id (int), value: counter object
+        self.__id = 0
+
+    def allocate_counter(self, number_of_luggages, fellows) -> int:
+        # algorithm goes here
+        counter_ids = self.__counters.keys()
+        return choice(list(counter_ids)) if len(counter_ids) > 0 else None
+
+    def add(self, staff_id: str) -> int:
+        self.__id += 1
+        counter = Counter(self.__id, staff_id, True, 0)
+        self.__counters[counter.counter_id] = counter
+        return counter.counter_id
+
+    def remove(self, counter: Counter):
+        del self.__counters[counter.counter_id]
+
+    def length_add(self, counter_id: int):
+        counter = self.__counters[counter_id]
+        counter.length += 1
+
+    def length_sub(self, staff_id: str):
+        counter = self.find_by_staff(staff_id)
+        counter.length -= 1
+
+    def find_by_staff(self, staff_id: str) -> Counter:
+        return next(filter(lambda counter: counter.staff_id == staff_id, self.__counters.values()))
+
+
+counter_service = CounterService()
